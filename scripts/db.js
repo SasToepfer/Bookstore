fe70f4cb55ca1670309dc7982e29e1b273453d35
SasToepfer <sas.toepfer@gmail.com>
2024-10-31 10:30:50 +0100

    first commit

---
+++ scripts/db.js
@@ -0,0 +1,157 @@
+let games = [
+    {
+        "name": "Die Legende von Arcania",
+        "author": "Max Schreiber",
+        "likes": 3400,
+        "liked": true,
+        "price": 49.99,
+        "publishedYear": 2020,
+        "genre": "RPG",
+        "comments": [
+            {
+                "name": "GameMaster99",
+                "comment": "Ein episches Rollenspiel mit fesselnder Story und tollen Charakteren."
+            },
+            {
+                "name": "MagicFan",
+                "comment": "Die Zauberfähigkeiten und offene Welt sind atemberaubend!"
+            },
+            {
+                "name": "AdventureAddict",
+                "comment": "Eine wunderschöne Welt voller Abenteuer und Geheimnisse!"
+            },
+            {
+                "name": "RPG_Guru",
+                "comment": "Ein Muss für jeden RPG-Fan. Die Kämpfe sind herausfordernd und lohnend."
+            },
+            {
+                "name": "StorySeeker",
+                "comment": "Eine packende Geschichte mit unerwarteten Wendungen."
+            }
+        ]
+    },
+    {
+        "name": "Galaxiensturm",
+        "author": "Lea Kosmos",
+        "likes": 2250,
+        "liked": false,
+        "price": 39.99,
+        "publishedYear": 2022,
+        "genre": "Sci-Fi Shooter",
+        "comments": [
+            {
+                "name": "LaserLord",
+                "comment": "Faszinierende Weltraumschlachten und realistische Physik!"
+            },
+            {
+                "name": "SciFiHero",
+                "comment": "Für Fans von Raumschiffen und Sci-Fi eine klare Empfehlung."
+            },
+            {
+                "name": "StarGazer",
+                "comment": "Die Grafik ist umwerfend und das Gameplay macht süchtig."
+            },
+            {
+                "name": "ShooterKing",
+                "comment": "Das beste Sci-Fi-Actionspiel, das ich je gespielt habe!"
+            },
+            {
+                "name": "GamerGirl99",
+                "comment": "Die Story ist ein wenig flach, aber das Gameplay rockt!"
+            }
+        ]
+    },
+    {
+        "name": "Verloren im Schatten",
+        "author": "Finn Dunkel",
+        "likes": 1890,
+        "liked": true,
+        "price": 29.99,
+        "publishedYear": 2019,
+        "genre": "Horror",
+        "comments": [
+            {
+                "name": "DarkSoul",
+                "comment": "Eine düstere Atmosphäre, die Gänsehaut garantiert."
+            },
+            {
+                "name": "SpookyFan",
+                "comment": "Die Soundeffekte sind furchterregend gut!"
+            },
+            {
+                "name": "ShadowHunter",
+                "comment": "Perfekt für Fans des Überlebenshorrors, nichts für schwache Nerven."
+            },
+            {
+                "name": "GhostlyGamer",
+                "comment": "Ein Horror-Meisterwerk mit spannender Story und dunklen Rätseln."
+            },
+            {
+                "name": "FearSeeker",
+                "comment": "Hat mich einige Male echt erschreckt. Toller Nervenkitzel!"
+            }
+        ]
+    },
+    {
+        "name": "Baumeister der Zukunft",
+        "author": "Ina Architekt",
+        "likes": 1575,
+        "liked": false,
+        "price": 24.99,
+        "publishedYear": 2021,
+        "genre": "Simulation",
+        "comments": [
+            {
+                "name": "CityDreamer",
+                "comment": "Super Stadt-Simulation mit vielen Möglichkeiten."
+            },
+            {
+                "name": "BuilderBro",
+                "comment": "Die Details bei der Stadtgestaltung sind beeindruckend!"
+            },
+            {
+                "name": "EcoGamer",
+                "comment": "Ein tolles Spiel für nachhaltiges Bauen und Planen."
+            },
+            {
+                "name": "FuturePlanner",
+                "comment": "Bietet mehr Kreativität und Freiheit als erwartet."
+            },
+            {
+                "name": "ArchitectLady",
+                "comment": "Perfekt, um die Städte der Zukunft zu entwerfen."
+            }
+        ]
+    },
+    {
+        "name": "Zeit des Kriegers",
+        "author": "Kai Stahl",
+        "likes": 2900,
+        "liked": true,
+        "price": 44.99,
+        "publishedYear": 2023,
+        "genre": "Action",
+        "comments": [
+            {
+                "name": "ActionAddict",
+                "comment": "Intensive Kämpfe und unglaubliche Grafik!"
+            },
+            {
+                "name": "BattleFreak",
+                "comment": "Ein echter Adrenalinschub. Action ohne Ende!"
+            },
+            {
+                "name": "WarriorSoul",
+                "comment": "Die Kampfsysteme und Moves sind beeindruckend."
+            },
+            {
+                "name": "CombatKing",
+                "comment": "Das beste Actionspiel, das ich seit langem gespielt habe."
+            },
+            {
+                "name": "EpicGamer",
+                "comment": "Großartiges Setting und packende Storyline."
+            }
+        ]
+    }
+]
